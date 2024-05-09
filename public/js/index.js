3c7b931fc3e4b8b92d8edc8c3e6cfac888f8398d
Yashgit1203 <yg63852@gmail.com>
2024-05-09 22:32:48 +0530

    Update Features

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,38 @@
 let taxswitch = document.getElementById("flexSwitchCheckDefault");
-  taxswitch.addEventListener("click",()=>{
-    let taxicons = document.getElementsByClassName("tax-icon");
-    for (taxicon of taxicons){
-      if(taxicon.style.display != "inline"){
-        taxicon.style.display = "inline";
-      }
-      else{
-        taxicon.style.display = "none";
-      }
 
+let cprices = document.querySelectorAll(".card-price");
+const arrs=[];
+  for(cprice of cprices){
+    arrs.push(cprice.textContent)
+  }
+taxswitch.addEventListener("click", () => {
+  
+  cprices.forEach(cardprice => {
+    // Conversion from local string to number
+    const formattedNumber = cardprice.textContent; 
+
+    // Remove commas from the formatted number
+    const unformattedNumber = formattedNumber.replace(/,/g, '');
+
+    let number = parseInt(unformattedNumber);
+
+    let amt_gst = (number * 18) / 100;
+    let total_amt=0;
+    if (taxswitch.checked) {
+      total_amt = number + amt_gst;
+    } else {
+      for(let i=0;i<arrs.length;i++){
+        cprices[i].textContent=arrs[i];
+      }      
     }
-  })
+    
+    // Conversion from number to local String
+    let converted_amt = total_amt.toLocaleString("en-IN");
+    // Reflecting changes
+    cardprice.textContent = converted_amt;
+  });
+});
+
 
   let scrollLeftbtn = document.querySelector(".scroll-left-btn");
   let scrollRightbtn = document.querySelector(".scroll-right-btn");
